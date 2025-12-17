a63e755e7dcfbd315a2329b1c174191b0e685fe3
gdoyle87 <gary.doyle@gmail.com>
2025-12-17 10:13:58 -0800

    completed part 1 and copied/updated logic for part 2

--- 2025/days/day08.py
+++ 2025/days/day08.py
@@ -1,9 +1,12 @@
+import math
 from collections import namedtuple
 
 from utils.loader import load_lines
 
 Point = namedtuple("Point", ["x", "y", "z"])
 Edge = namedtuple("Edge", ["distance", "i", "j"])
+
+CONNECTIONS_NEEDED = 1000
 
 
 def part1(data):
@@ -16,12 +19,94 @@ def part1(data):
             distance_sq = (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2
             edges.append(Edge(distance_sq, i, j))
     edges.sort()
-    print(edges)
-    return
+
+    parent = list(range(n))  # initialize each as its own parent
+    circuit_sizes = [1] * n  # each circuit begins as size 1
+
+    def union(i, j):
+        def find(i):
+            if parent[i] == i:
+                return i
+            parent[i] = find(parent[i])
+            return parent[i]
+
+        root_i = find(i)
+        root_j = find(j)
+
+        if root_i != root_j:
+            #            if circuit_sizes[root_i] < circuit_sizes[root_j]:
+            #                root_i, root_j = root_j, root_i
+
+            # update the parent of j to be parent of i
+            parent[root_j] = root_i
+            # update the size of circuit i to also include the size of circuit j
+            circuit_sizes[root_i] += circuit_sizes[root_j]
+            return True
+        return False
+
+    # combine edges starting from shortest, stop once we have reached the required number of connections
+    connections = 0
+    for edge in edges:
+        union(edge.i, edge.j)
+        connections += 1
+        if connections == CONNECTIONS_NEEDED:
+            break
+
+    # circuit_sizes includes sizes for every point in edges, even when they are not a parent point.
+    # Therefore we need to filter out nodes where the circuit is not a parent.
+    final_sizes = [circuit_sizes[i] for i in range(n) if parent[i] == i]
+
+    # we want only the top 3 size circuits so sort desc
+    final_sizes.sort(reverse=True)
+    return math.prod(final_sizes[:3])
 
 
 def part2(data):
-    pass
+    n = len(data)
+
+    edges = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            p1, p2 = data[i], data[j]
+            distance_sq = (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2
+            edges.append(Edge(distance_sq, i, j))
+    edges.sort()
+
+    parent = list(range(n))  # initialize each as its own parent
+    circuit_sizes = [1] * n  # each circuit begins as size 1
+
+    def union(i, j):
+        def find(i):
+            if parent[i] == i:
+                return i
+            parent[i] = find(parent[i])
+            return parent[i]
+
+        root_i = find(i)
+        root_j = find(j)
+
+        if root_i != root_j:
+            #            if circuit_sizes[root_i] < circuit_sizes[root_j]:
+            #                root_i, root_j = root_j, root_i
+
+            # update the parent of j to be parent of i
+            parent[root_j] = root_i
+            # update the size of circuit i to also include the size of circuit j
+            circuit_sizes[root_i] += circuit_sizes[root_j]
+            return True
+        return False
+
+    # combine edges starting from shortest, stop once we have connected all points (needs n - 1 connections)
+    connections = 0
+    last_edge = None
+    for edge in edges:
+        if union(edge.i, edge.j):
+            connections += 1
+            if connections == n - 1:
+                last_edge = edge
+                break
+    print(last_edge)
+    return
 
 
 def solve():
