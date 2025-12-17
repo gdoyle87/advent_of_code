206837a677939bc09cf0f2a8a0c87276552b9b75
gdoyle87 <gary.doyle@gmail.com>
2025-12-17 09:19:45 -0800

    imported the sample data, converted it to a list of namedtuple points, and then calculated a list of all the distances between pairs of points

---
+++ 2025/days/day08.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+
+from utils.loader import load_lines
+
+Point = namedtuple("Point", ["x", "y", "z"])
+Edge = namedtuple("Edge", ["distance", "i", "j"])
+
+
+def part1(data):
+    n = len(data)
+
+    edges = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            p1, p2 = data[i], data[j]
+            distance_sq = (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2
+            edges.append(Edge(distance_sq, i, j))
+    print(edges)
+    return
+
+
+def part2(data):
+    pass
+
+
+def solve():
+    data = load_lines(8)
+    points = []
+    for point in data:
+        x, y, z = map(int, point.split(","))
+        points.append(Point(x, y, z))
+
+    return part1(points), part2(points)
