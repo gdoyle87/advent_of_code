ff0b550112c1e7f1248c235e443bbc317873bb56
gdoyle87 <gary.doyle@gmail.com>
2025-12-18 12:40:43 -0800

    updated the data to be ints so that the masking can be xored once rather than on a bit by bit basis

--- 2025/days/day10.py
+++ 2025/days/day10.py
@@ -2,9 +2,7 @@ from collections import namedtuple
 
 from utils.loader import load_lines
 
-Machine = namedtuple(
-    "Machine", ["starting_state", "light_diagram", "buttons", "joltage"]
-)
+Machine = namedtuple("Machine", ["diagram_len", "light_diagram", "buttons", "joltage"])
 
 
 def part1(data):
@@ -20,25 +18,33 @@ def solve():
     remove_chars = "{}[]()"
     parsed_data = []
     for row in data:
+        # remove brackets
         table = str.maketrans("", "", remove_chars)
         result = row.translate(table).split()
+
+        # unpack the result
         raw_light_diagram, *raw_buttons, raw_joltage = result
+
         diagram_len = len(raw_light_diagram)
+
+        target_int = 0
+        for i, char in enumerate(raw_light_diagram):
+            if char == "#":
+                target_int |= 1 << i
+
         button_masks = []
         for btn_str in raw_buttons:
-            mask = [0] * diagram_len
+            mask = 0
             indices = map(int, btn_str.split(","))
             for idx in indices:
-                if 0 <= idx < diagram_len:
-                    mask[idx] = 1
+                mask |= 1 << idx
             button_masks.append(mask)
 
         machine = Machine(
-            starting_state=[0] * diagram_len,
-            light_diagram=[0 if char == "." else 1 for char in raw_light_diagram],
+            diagram_len=diagram_len,
+            light_diagram=target_int,
             buttons=button_masks,
             joltage=set(map(int, raw_joltage.split(","))),
         )
         parsed_data.append(machine)
-    data = parsed_data
-    return part1(data), part2(data)
+    return part1(parsed_data), part2(parsed_data)
