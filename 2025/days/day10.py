c197eed4b8f834dd8235d9343460075c6aae9dbe
gdoyle87 <gary.doyle@gmail.com>
2025-12-18 12:28:14 -0800

    finished preparing the data as a named tuple containting starting state, light diagram, a list of buttons, and a joltage. the state and diagram are lists of 0s and 1s, buttons are masks of the diagram size showing which is flipped

--- 2025/days/day10.py
+++ 2025/days/day10.py
@@ -1,11 +1,44 @@
+from collections import namedtuple
+
 from utils.loader import load_lines
 
+Machine = namedtuple(
+    "Machine", ["starting_state", "light_diagram", "buttons", "joltage"]
+)
+
+
 def part1(data):
-    pass
+    print(data)
+
 
 def part2(data):
     pass
 
+
 def solve():
     data = load_lines(10)
+    remove_chars = "{}[]()"
+    parsed_data = []
+    for row in data:
+        table = str.maketrans("", "", remove_chars)
+        result = row.translate(table).split()
+        raw_light_diagram, *raw_buttons, raw_joltage = result
+        diagram_len = len(raw_light_diagram)
+        button_masks = []
+        for btn_str in raw_buttons:
+            mask = [0] * diagram_len
+            indices = map(int, btn_str.split(","))
+            for idx in indices:
+                if 0 <= idx < diagram_len:
+                    mask[idx] = 1
+            button_masks.append(mask)
+
+        machine = Machine(
+            starting_state=[0] * diagram_len,
+            light_diagram=[0 if char == "." else 1 for char in raw_light_diagram],
+            buttons=button_masks,
+            joltage=set(map(int, raw_joltage.split(","))),
+        )
+        parsed_data.append(machine)
+    data = parsed_data
     return part1(data), part2(data)
